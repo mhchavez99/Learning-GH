41d3708a66e866d55b42e1eb7597d80c2d0ff56d
Michael Chavez <mhchavez99@gmail.com>
2021-05-30 23:26:39 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Michael Chavez on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
